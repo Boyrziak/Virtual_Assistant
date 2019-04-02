6167b20d664a5af41ab1f1939f744cf8eec7b097
m.borziak <m.borziak@intetics.com>
2019-04-02 19:28:58 +0300

    Cookie methods

--- app.js
+++ app.js
@@ -249,6 +249,45 @@ jQuery(document).ready(function ($) {
                 $(body).animate({left: windowWidth - body.outerWidth() - 5 + 'px'}, 500);
             }
         },
+        setCookie: function (name, value, options) {
+            options = options || {};
+
+            var expires = options.expires;
+
+            if (typeof expires == "number" && expires) {
+                var d = new Date();
+                d.setTime(d.getTime() + expires * 1000);
+                expires = options.expires = d;
+            }
+            if (expires && expires.toUTCString) {
+                options.expires = expires.toUTCString();
+            }
+
+            value = encodeURIComponent(value);
+
+            var updatedCookie = name + "=" + value;
+
+            for (var propName in options) {
+                updatedCookie += "; " + propName;
+                var propValue = options[propName];
+                if (propValue !== true) {
+                    updatedCookie += "=" + propValue;
+                }
+            }
+
+            document.cookie = updatedCookie;
+        },
+        getCookie: function (name) {
+            var matches = document.cookie.match(new RegExp(
+                "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+            ));
+            return matches ? decodeURIComponent(matches[1]) : undefined;
+        },
+        deleteCookie: function (name) {
+            setCookie(name, "", {
+                expires: -1
+            })
+        },
         addMessage: function (messageDto) {
             const type = messageDto.message.type;
             const sender = messageDto.senderType;
